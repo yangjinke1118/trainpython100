2123ddd9e4d92ea8b3a716cf4ea2cdad1f6dbfa6
yangjinke1118 <yangjinke1118@163.com>
2019-09-17 22:14:24 +0800

    完成 train002 代码，本实例练习了for循环和range()

--- train002.py
+++ train002.py
@@ -5,37 +5,71 @@
 高于60万元的部分，可提成1.5%，高于100万元时，超过100万元的部分按1%提成，从键盘输入当月利润I，
 求应发放奖金总数？
 程序分析：请利用数轴来分界，定位。注意定义时需把奖金定义成长整型。
+
+程序源代码：
+实例(Python 2.0+)
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+i = int(raw_input('净利润:'))
+arr = [1000000,600000,400000,200000,100000,0]
+rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+r = 0
+for idx in range(0,6):
+    if i>arr[idx]:
+        r+=(i-arr[idx])*rat[idx]
+        print (i-arr[idx])*rat[idx]
+        i=arr[idx]
+print r
 """
-reward_set = [10, 20, 40, 60, 100]
-percent_rate = [10, 7.5, 5, 3, 1.5, 1]
+
+#  自己实现
+
 
 def get_reward_range(reward_set):
     reward_range = []
     for i in range(len(reward_set)):
         if i == 0:
-            reward_range[i] = reward_set[i]
+            reward_range.append(reward_set[i])
         else:
-            reward_range[i] = reward_set[i] - reward_set[i-1]
+            reward_range.append(reward_set[i] - reward_set[i-1])
     return reward_range
 
-def get_reward(money, reward_range, percent_rate):
+
+def get_reward(money, reward_set, percent_rate):
+    reward_range = get_reward_range(reward_set)
     reward = 0.0
-    if money <= 100:
+    _money = money
+    if money == 100:  # 输入的money=100
         for i, num in enumerate(reward_range, 0):
-            if money <= num:
-                reward += money * percent_rate[i]
-                break
-            else:
-                reward += num * percent_rate[i]
-                money -= num
-    else:
+            reward += num * percent_rate[i]
+    elif money < 100:
         for i, num in enumerate(reward_range, 0):
-            if money <= num:
-                reward += money * percent_rate[i]
-                break
-            else:
+            if _money > num:
                 reward += num * percent_rate[i]
-                money -= num
+                _money -= num
+            elif _money <= num:
+                reward += _money * percent_rate[i]
+                break
+    elif money > 100:
+        for i, num in enumerate(reward_range, 0):
+            reward += num * percent_rate[i]
+            _money -= num
+        reward += _money * percent_rate[i+1]
+    return reward
+
+
+reward_set = [10, 20, 40, 60, 100]
+percent_rate = [10, 7.5, 5, 3, 1.5, 1]
+
+
+print('获得的奖金为:', get_reward(int(input('请输入利润金额:')), reward_set, percent_rate)/100)
+
+input()
+
+"""
+示例代码程序更精简。
+"""
 
 
 
